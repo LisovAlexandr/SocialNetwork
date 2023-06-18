8c91d10de44c0723eb0b9593efa250edac9cfd0c
Alexander <lisov.alexander@mail.ru>
2023-06-18 21:10:05 +0300

    add main blocks

--- src/App.js
+++ src/App.js
@@ -2,31 +2,18 @@ import './App.css';
 
 const App=()=>{
   return(
-    <div>
-     <Header/>
-     <Footer/>
+    <div className='app-wrapper'>
+     <header>
+      <img src='./../public/logo192.png' alt='icony'/>
+     </header>
+     <nav>
+      <div>Profile</div>
+      <div>Messages</div>
+     </nav>
+     <footer>
+      Main content
+     </footer>
     </div>
     )
 }
-
-const Header =()=> {
-  return (
-    <div className="App">
-      <ul>
-        <li>это хэдер</li>
-        <li>2</li>
-      </ul>
-     
-    </div>
-  );
-}
-
-const Footer=()=>{
-  return(
-    <div>
-    text from new Footer
-    </div>
-    )
-}
-
 export default App;
