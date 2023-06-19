5d69d58e832031a4141613652920702a533a1508
Alexander <lisov.alexander@mail.ru>
2023-06-20 00:30:12 +0300

    add all component in App.js

--- src/App.js
+++ src/App.js
@@ -24,10 +24,29 @@ const App=()=>{
       </div>
 
      </nav>
-     <footer className='content'>
-      <img alt='main' src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
+     <div className='content'>
+      <div>
+        <img alt='main' src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
+      </div>
+      <div>
+        ava+description
+      </div>
+      <div>
+        My posts
+        <div>
+          new post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
       Main content
-     </footer>
+     </div>
     </div>
     )
 }
